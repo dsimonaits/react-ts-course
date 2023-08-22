6b94d551b5c297519e57762ed9066239e26795ce
Deniss Simonaits <dsimonaits@gmail.com>
2023-08-23 01:18:54 +0300

    added custom hook for fetching data

---
+++ src/hooks/useFetching.ts
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+type FetchingHookResult = [() => Promise<void>, boolean, string];
+
+const useFetching = (callback: () => Promise<void>): FetchingHookResult => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const fetching = async () => {
+    try {
+      setIsLoading(true);
+      await callback();
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return [fetching, isLoading, error];
+};
+
+export default useFetching;
