6c66bc9d22fdc01704d6bd90d94cecdf0e7c92bc
Deniss Simonaits <dsimonaits@gmail.com>
2023-08-23 19:23:20 +0300

    added comments for explanation and better understanding

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -7,18 +7,27 @@ import List from "../components/UI/List/List";
 import UserCard from "../components/UI/UserCard/UserCard";
 
 const UserPage = () => {
+  // Define state for users and specify the type as an array of IUser.
   const [users, setUsers] = useState<IUser[]>([]);
-
+  //
+  // Use the custom useFetching hook to handle data fetching.
+  // Here, we specify that the callback function will return a Promise with AxiosResponse.
   const [fetchUsers, isLoading, usersError] = useFetching(async () => {
+    //
+    // Fetch all users and expect the response to be of type AxiosResponse<IUser[]>.
     const response: AxiosResponse<IUser[]> | undefined = await fetchAllUsers();
+    //
+    // Set the users state to the data received from the response or an empty array if undefined.
     setUsers(response?.data || []);
   });
 
+  // Use the useEffect hook to fetch users when the component mounts.
   useEffect(() => {
     fetchUsers();
   }, []);
 
   return (
+    // Render a List component and provide it with the users data.
     <List
       items={users}
       renderItem={(user: IUser) => <UserCard user={user} key={user.id} />}
