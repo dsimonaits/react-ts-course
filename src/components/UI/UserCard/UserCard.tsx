9c1038b77c46bf64ea0d15b45b4177e6c64f55eb
Deniss Simonaits <dsimonaits@gmail.com>
2023-08-23 19:37:01 +0300

    added comments with explanations

--- src/components/UI/UserCard/UserCard.tsx
+++ src/components/UI/UserCard/UserCard.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from "react";
 import { IUser } from "../../../types/types";
 import cl from "./UserCard.module.css";
+
+// Define an interface UserCardProps to specify the expected prop structure.
 interface UserCardProps {
-  user: IUser;
+  user: IUser; // The 'user' prop should be of type IUser.
 }
 
+// Create a functional component UserCard that takes UserCardProps as props.
 const UserCard: FC<UserCardProps> = ({ user }) => {
+  // Render a user card component with the provided 'user' prop.
+
   return (
     <div className={cl.user__card}>
+      {/* Render the user's profile image, with a fallback to "default-user.png" if 'user.profileImageUrl' is falsy. */}
       <img
         src={user.profileImageUrl || "default-user.png"}
         alt="User Profile"
         className={cl.user__image}
       />
+
       <div className={cl.user__info}>
+        {/* Display the user's name, email, and website using data from 'user'. */}
         <h2>{user.name}</h2>
         <p>{user.email}</p>
         <p>{user.website}</p>
