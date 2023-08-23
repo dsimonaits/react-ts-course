5b62b1b55a65dfd7e323f27bfb22ceb3905f871c
Deniss Simonaits <dsimonaits@gmail.com>
2023-08-23 21:33:18 +0300

    base url react-ts-userCards

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/react-ts-userCards/",
   plugins: [react()],
+  build: {
+    rollupOptions: {},
+  },
 });
