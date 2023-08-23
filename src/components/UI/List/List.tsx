f4120df5ebd82ed2b0c959f76219993d6af40bff
Deniss Simonaits <dsimonaits@gmail.com>
2023-08-23 14:22:59 +0300

    UI folder for reusable components

---
+++ src/components/UI/List/List.tsx
@@ -0,0 +1,13 @@
+import React, { FC, PropsWithChildren } from "react";
+import cl from "./List.module.css";
+
+interface ListProps<T> extends PropsWithChildren {
+  items: T[];
+  renderItem: (item: T) => React.ReactNode;
+}
+
+function List<T>(props: ListProps<T>) {
+  return <ul className={cl.list}>{props.items.map(props.renderItem)}</ul>;
+}
+
+export default List;
---
+++ src/components/UI/UserCard/UserCard.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from "react";
+import { IUser } from "../../../types/types";
+import cl from "./UserCard.module.css";
+
+interface UserCardProps {
+  user: IUser;
+}
+
+const UserCard: FC<UserCardProps> = ({ user }) => {
+  return (
+    <div className={cl.user__card}>
+      <img
+        src={user.profileImageUrl || "/default-user.png"}
+        alt="User Profile"
+        className={cl.user__image}
+      />
+      <div className={cl.user__info}>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
